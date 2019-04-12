19445c216b9aef17bcb68f7bd523a4a8c18fbf1e
robmachado <linux.rlm@gmail.com>
2019-04-12 16:41:03 -0300

    [UPD] sec3

--- main.js
+++ main.js
@@ -5,18 +5,25 @@ const url = require('url');
 const path = require('path');
 
 let mainWindow;
-process.env.NODE_ENV = "dev";
+process.env.NODE_ENV = "production";
+
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         width: 800,
-        height: 500,
+        height: 570,
         minWidth: 800,
-        minHeight: 500,
+        minHeight: 570,
+        titleBarStyle: 'hidden',
+        title: 'cadlabel',
+        skipTaskbar: true,
+        toolbar: false,
         icon: path.join(__dirname, 'icon.png')
     });
+
     mainWindow.loadURL(`file://${__dirname}/index.html`);
     mainWindow.setMenu(null);
+    mainWindow.setMenuBarVisibility(false);
 
     if (process.env.NODE_ENV != "production") {
         mainWindow.webContents.openDevTools();
